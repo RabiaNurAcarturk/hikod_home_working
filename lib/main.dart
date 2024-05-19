0057fc24867fde0f26582eba9bcafe656831cfd3
Rabia Nur Acartürk <rabianur26006@gmail.com>
2024-05-19 23:46:59 +0300

    ilk commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 void main() {
+   //a secenegi
+   int yas=24;
+   String gun='pazar';
+   double goz numarasi=5.25;
+   bool haftalikhedef=true;
 
-  var ilkDegiskenim=20;
-   print(ilkDegiskenim);
+   //b secenegi
+   bool deveHorgucu=true;
+   bool PascalınDurumu=true;
+   bool yılanın_durumu=true;
+
+   //c secenegi
+   var ad='Rabia Nur';
+    var soyad='ACARTÜRK';
+    print(ad+soyad);
+    var yasim=24;
+     print(yasim);
+     bool resitlikdurumum=true;
 }
